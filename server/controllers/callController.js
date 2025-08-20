5405687bcbbebd56393258a694c5edc92c229993
Davy Batista <davybatista@192.168.0.22>
2025-08-19 21:48:17 -0300

    refactor: reorganize project into server and devices folders

--- server/controllers/callController.js
+++ server/controllers/callController.js
@@ -1,7 +1,14 @@
 const callModel = require('../models/callModel');
 
 function novaChamada(data, io) {
-    const added = callModel.addCall(data);
+
+    const chamadaComData = {
+        ...data,
+        timestamp: new Date().toISOString() // formato ISO (ex: 2025-08-19T22:30:00.000Z)
+    };
+    console.log(chamadaComData)
+
+    const added = callModel.addCall(chamadaComData);
     if (added) {
         io.emit('novaChamada', data);
     }
